7872bc83ff3154a6bd3d19bd69ce2d9eb514c1fb
eun1310434 <eun1310434@naver.com>
2018-03-16 19:08:54 +0900

    -

---
+++ src/com/eun1310434/database/connectionfactory/DBGetData.java
@@ -0,0 +1,142 @@
+/*=====================================================================
+□ Infomation
+  ○ Data : 15.03.2018
+  ○ Mail : eun1310434@naver.com
+  ○ Blog : https://blog.naver.com/eun1310434
+  ○ Reference : 쉽게 배우는 소프트웨어 공학, Java Documentation, 헬로 자바 프로그래밍, programmers.co.kr, 
+
+□ Function
+  ○ 데이터 출력을 위한 명령어 모음
+  
+□ Study      
+  ○ DataBase
+    - 데이터를 관리하기 위해 만들어진 프로그램 (Oracle, MS-AQL, MySAL, Postgres 등)
+    - 데이터를 잘 관리(입력, 수정, 삭제, 검색 등) 하려고 만든 서버 프로그램
+    - 질의문(Query) : 데이터베이스에서 데이터를 관리 할 때 사용하는 명령문
+    - 데이터베이스 생성
+       01) 데이터베이스 접속
+       02) show databases
+       03) create database 데이터베이스명
+       04) drop database 데이터베이스명
+       05) use 데이터베이스명
+    - 테이블 생성
+       01) create table 테이블명(필드1 자료형, 필드2 자료형, 필드3 자료형,...)
+            : 자바와 반대의 순서로 필드 선언
+       02) show tables
+       03) desc 테이블명
+    - 자바 자료형 / MySql 자료형
+       01) boolean / tinyint
+       02) byte / tinyint
+       03) char / char, varchar
+       04) short / smallint
+       05) int / int, integer
+       06) long / bigint
+       07) float / float
+       08) double / double
+       09) String / char(n), varchar(n), BLOB, text
+       10) Date / date
+       11) DateTime / datetime
+       12) Timestamp / timestamp
+       13) Time / time
+    - 질의문
+      01) 입력 질의문 
+            : insert into 테이블이름 values (데이터1, 데이터2, 데이터3, 데이터4, ...)
+      02) 출력(검색) 질의문
+            : select * from 테이블이름
+      03) 수정 질의문
+            : update 테이블이름 set 타이틀1 = 값1, 타이틀2 = 값2, ...
+      04) 삭제 질의문
+            : delete from 테이블이름
+
+  ○ JDBC
+    - Java DataBase Connectivity
+    - 자바와 데이터베이스를 연결하여 데이터의 저장, 수정, 삭제, 검색등의 작업을 지원
+
+  ○ JDBC Driver
+    - 자바와 데이터베이스를 연결하기 위한 드라이버 프로그램
+    - JAVA MySQL Setting
+      01) mysql-connector-java 다운받기
+      02) C:\Program Files\Java\jdk1.8.0_73\jre\lib\ext에 파일 붙여넣기
+
+  ○ JDBC 프로그램 작성 방법
+      01) 자바의 JDBC API 인터페이스를 구현한 드라이버 클래스를 찾아 객체를 생성한다.
+      02) DBMS와의 연결을 관리하는 Connection 객체를 생성한다.
+      03) 질의 작업을 처리하는 Statement, PreparedStatement, CallableStatement 객체를 생성한다.
+      04) ResultSet객체를 통해 질의 결과를 획득하고 처리한다.
+      05) DBMS와의 연결을 종료한다.
+    - URL
+             프로토콜 : 서브프로토콜 : SID
+      JDBC : MySQL : SID
+
+  ○ 커넥션 팩토리
+      - Client가 요청시 Connection을 각각에 맞춰 생성 후 종료
+      - JDBC 프로그램을 작성하면서 반복적으로 코드를 작성해야 하는 블록을 모듈화하여 동일한 코드를 찍어 낼 수 있도록 사용하는 패턴(Pattern)
+
+  ○ 커넥션 풀
+      - Client가 요청시 기존 사용중인 Connection을 재사용
+      - DBC와의 커넥션을 생성할 때 발생하는 부하를 줄이고자 한번 사용한 커넥션 객체를 삭제하지 않고 적절한 수준으로 특정 영역에 연결객체를 관리하는 모듈.
+
+  ○ 라운드 마무리
+      - JDBC란?
+      - 데이터베이스란?
+      - JDBC 프로그램을 작성하는 순서는?
+      - 커넥션 팩토리와 커넥션 풀의 용도는?
+=====================================================================*/
+package com.eun1310434.database.connectionfactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBGetData {
+	
+	public void getUserInfo(Connection connection, String user_sn){
+		//01) 자바의 JDBC API 인터페이스를 구현한 드라이버 클래스를 찾아 객체를 생성한다. → DBConnectionFactory에서 처리
+		//02) DBMS와의 연결을 관리하는 Connection 객체 생성 → DBConnectionFactory에서 처리
+		
+		//Setting
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			//03) 질의 작업을 처리하는 Statement, PreparedStatement, CallableStatement 객체 생성
+			String query = "select * from USER where user_sn = ?";
+			preparedStatement = connection.prepareStatement(query); 
+			preparedStatement.setString(1, user_sn);
+			
+			//04) ResultSet객체를 통해 질의 결과를 획득하고 처리
+			resultSet = preparedStatement.executeQuery();
+
+			//결과문 받기
+			resultSet.next();
+			String user_password = resultSet.getString("user_password");
+			String user_phone = resultSet.getString("user_phone");
+			String user_gcmaddress = resultSet.getString("user_gcmaddress");
+			String system_sn = resultSet.getString("system_sn");
+			String user_type = resultSet.getString("user_type");
+			String user_name = resultSet.getString("user_name");
+			String user_profile = resultSet.getString("user_profile");
+			
+			System.out.println("connection = " + connection);
+			System.out.println("user_sn = " + user_sn);
+			System.out.println("user_password = " + user_password);
+			System.out.println("user_phone = " + user_phone);
+			System.out.println("user_gcmaddress = " + user_gcmaddress);
+			System.out.println("system_sn = " + system_sn);
+			System.out.println("user_type = " + user_type);
+			System.out.println("user_name = " + user_name);
+			System.out.println("user_profile = " + user_profile);
+			
+
+			resultSet.close();
+
+			preparedStatement.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ src/com/eun1310434/database/connectionpool/DBGetInstance.java
@@ -0,0 +1,149 @@
+/*=====================================================================
+□ Infomation
+  ○ Data : 15.03.2018
+  ○ Mail : eun1310434@naver.com
+  ○ Blog : https://blog.naver.com/eun1310434
+  ○ Reference : 쉽게 배우는 소프트웨어 공학, Java Documentation, 헬로 자바 프로그래밍, programmers.co.kr, 
+
+□ Function
+  ○ 파일입출력을 통한 DB계정 획득 및 Connection 객체 생성  
+  
+□ Study      
+  ○ DataBase
+    - 데이터를 관리하기 위해 만들어진 프로그램 (Oracle, MS-AQL, MySAL, Postgres 등)
+    - 데이터를 잘 관리(입력, 수정, 삭제, 검색 등) 하려고 만든 서버 프로그램
+    - 질의문(Query) : 데이터베이스에서 데이터를 관리 할 때 사용하는 명령문
+    - 데이터베이스 생성
+       01) 데이터베이스 접속
+       02) show databases
+       03) create database 데이터베이스명
+       04) drop database 데이터베이스명
+       05) use 데이터베이스명
+    - 테이블 생성
+       01) create table 테이블명(필드1 자료형, 필드2 자료형, 필드3 자료형,...)
+            : 자바와 반대의 순서로 필드 선언
+       02) show tables
+       03) desc 테이블명
+    - 자바 자료형 / MySql 자료형
+       01) boolean / tinyint
+       02) byte / tinyint
+       03) char / char, varchar
+       04) short / smallint
+       05) int / int, integer
+       06) long / bigint
+       07) float / float
+       08) double / double
+       09) String / char(n), varchar(n), BLOB, text
+       10) Date / date
+       11) DateTime / datetime
+       12) Timestamp / timestamp
+       13) Time / time
+    - 질의문
+      01) 입력 질의문 
+            : insert into 테이블이름 values (데이터1, 데이터2, 데이터3, 데이터4, ...)
+      02) 출력(검색) 질의문
+            : select * from 테이블이름
+      03) 수정 질의문
+            : update 테이블이름 set 타이틀1 = 값1, 타이틀2 = 값2, ...
+      04) 삭제 질의문
+            : delete from 테이블이름
+
+  ○ JDBC
+    - Java DataBase Connectivity
+    - 자바와 데이터베이스를 연결하여 데이터의 저장, 수정, 삭제, 검색등의 작업을 지원
+
+  ○ JDBC Driver
+    - 자바와 데이터베이스를 연결하기 위한 드라이버 프로그램
+    - JAVA MySQL Setting
+      01) mysql-connector-java 다운받기
+      02) C:\Program Files\Java\jdk1.8.0_73\jre\lib\ext에 파일 붙여넣기
+
+  ○ JDBC 프로그램 작성 방법
+      01) 자바의 JDBC API 인터페이스를 구현한 드라이버 클래스를 찾아 객체를 생성한다.
+      02) DBMS와의 연결을 관리하는 Connection 객체를 생성한다.
+      03) 질의 작업을 처리하는 Statement, PreparedStatement, CallableStatement 객체를 생성한다.
+      04) ResultSet객체를 통해 질의 결과를 획득하고 처리한다.
+      05) DBMS와의 연결을 종료한다.
+    - URL
+             프로토콜 : 서브프로토콜 : SID
+      JDBC : MySQL : SID
+
+  ○ 커넥션 팩토리
+      - Client가 요청시 Connection을 각각에 맞춰 생성 후 종료
+      - JDBC 프로그램을 작성하면서 반복적으로 코드를 작성해야 하는 블록을 모듈화하여 동일한 코드를 찍어 낼 수 있도록 사용하는 패턴(Pattern)
+
+  ○ 커넥션 풀
+      - Client가 요청시 기존 사용중인 Connection을 재사용
+      - DBC와의 커넥션을 생성할 때 발생하는 부하를 줄이고자 한번 사용한 커넥션 객체를 삭제하지 않고 적절한 수준으로 특정 영역에 연결객체를 관리하는 모듈.
+
+  ○ 라운드 마무리
+      - JDBC란?
+      - 데이터베이스란?
+      - JDBC 프로그램을 작성하는 순서는?
+      - 커넥션 팩토리와 커넥션 풀의 용도는?
+=====================================================================*/
+package com.eun1310434.database.connectionpool;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBGetInstance {
+	private File file;
+	private String driver;
+	private String url;
+	private String user;
+	private String password;
+	private Connection connection;
+	
+	private static DBGetInstance factory = new DBGetInstance();
+	private static int ConnectionCount = 0;
+	
+	private DBGetInstance() {
+		file = new File("setting.db");//한번만 실행됨
+		
+		loadData();
+	}
+	
+	private void loadData() {
+		try {
+			BufferedReader fin = new BufferedReader(new FileReader(file));
+			driver = fin.readLine();
+			url = fin.readLine();
+			user = fin.readLine();
+			password = fin.readLine();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getInstance() {
+		ConnectionCount++;
+		return factory.getConnection();
+	}
+	
+	public static void getConnectionCount(){
+		System.out.println("ConnectionCount : "+ConnectionCount);
+	}
+	
+	private Connection getConnection() {
+		try {
+			Class.forName(driver);
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		try {
+			connection = DriverManager.getConnection(url, user, password);
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return connection;
+	}
+}
